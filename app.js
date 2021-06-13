2a983bbc7933bade54e40d0ea7aa68b05d24b18e
chayan101 <chayan.g101@gmail.com>
2021-06-13 10:52:08 +0530

    version 1

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var socket = require('socket.io');
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+var server = app.listen('4000', ()=>{
+  console.log('server up and running....');
+});
+
+
+
+app.use(express.static('public'));
+
+
+var io = socket(server);
+
+//here socket = the particular socket established between client and server
+io.on('connection' , function(socket){
+  console.log(`connection established between server and client @ ${socket.id}`);
+
+  socket.on('chat', function(data){
+    console.log(data);
+    //io.sockets refers to all the sockets connected to the server
+    io.sockets.emit('chat', data);
+    io.sockets.emit('clear');
+  });
+
+  socket.on('typing' , (data)=>{
+    //socket is the socket connected at the moment && broadcast means sending msg to every other socket
+    socket.broadcast.emit('typing', data);
+  });
+});
